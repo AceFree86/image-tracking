f9eaa80802f8ee8e3dcd4392ed410495d6fe8bb9
Ruslan <gifler@me.comm>
2025-01-23 22:51:01 +0200

    model.scale.set(1, 1, 1);

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
   loader.load("https://acefree86.github.io/image-tracking/assets/models/box.gltf",
     (gltf) => {
       const model = gltf.scene;
-      model.scale.set(0.5, 0.5, 0.5);
+      model.scale.set(1, 1, 1);
       anchor.group.add(model);
     },
     (xhr) => {
