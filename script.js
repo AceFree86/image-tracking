f265eff294ed27718dddddecdf098c02f316f12a
Ruslan <gifler@me.comm>
2025-01-24 00:33:22 +0200

    2

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const mindarThree = new MindARThree({
     container: document.querySelector("#container"),
     imageTargetSrc:
-      "https://acefree86.github.io/image-tracking-2/assets/Image/targets.mind",
+      "https://acefree86.github.io/image-tracking/assets/Image/targets.mind",
   });
 
   const { renderer, scene, camera } = mindarThree;
