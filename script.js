7b49d6206c3f379831cde63ac295676d22e8849b
Ruslan <gifler@me.comm>
2025-01-23 22:49:29 +0200

    url box

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const anchor = mindarThree.addAnchor(0);
 
   const loader = new GLTFLoader();
-  loader.load("https://acefree86.github.io/image-tracking//assets/models/box.gltf",
+  loader.load("https://acefree86.github.io/image-tracking/assets/models/box.gltf",
     (gltf) => {
       const model = gltf.scene;
       model.scale.set(0.5, 0.5, 0.5);
