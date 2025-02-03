b780a942c0eef49acd7e3b1c38bf0d7f6fcfa784
Ruslan <gifler@me.comm>
2025-02-03 02:12:00 +0200

    Cake.glb

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Load the GLTF model
   const url =
-    "https://acefree86.github.io/image-tracking/assets/models/Cake_Anim.glb";
+    "https://acefree86.github.io/image-tracking/assets/models/Cake.glb";
   const loader = new GLTFLoader();
 
   loader.load(
